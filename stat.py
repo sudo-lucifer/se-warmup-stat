b64feb0fbd10558ad601cf0d6bf893420e246601
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-01-13 15:25:15 +0700

    Done

--- stat.py
+++ stat.py
@@ -2,13 +2,18 @@ import sys
 
 system_arv = sys.argv
 
-if (len(system_arv) < 1):
+if (len(system_arv) <= 1):
     print("Please specify the file name")
 else:
-    file = open(system_arv[1], "r")
-    data = file.readlines()
-    row = ["mean", "std", "min", "max"]
-    print("Statistics Summary")
-    for i in range(len(row)):
-        if (len(data) > i):
-            print(row[i] + ":", data[i].strip())
+    try:
+        file = open(system_arv[1], "r")
+        data = file.readlines()
+        row = ["mean", "std", "min", "max"]
+        print("Statistics Summary")
+        for i in range(len(row)):
+            if (len(data) > i):
+                print(row[i] + ":", data[i].strip())
+            else: 
+                print(row[i] + ": N/A")
+    except FileNotFoundError:
+        print("No such file")
