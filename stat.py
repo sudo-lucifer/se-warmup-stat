4b941140a61ccbf042af7d92c413c8eda602a2c3
karank85 <kkjas85@gmail.com>
2023-01-13 15:59:11 +0700

    added multiple file reading feature + added scale

--- stat.py
+++ stat.py
@@ -3,18 +3,38 @@ import numpy as np
 
 system_arv = sys.argv
 
+def print_data(lst):
+    print("mean:", np.mean(lst), end=' ')
+    print("std:", np.std(lst), end=' ')
+    print("min:", np.min(lst), end=' ')
+    print("max:", np.max(lst), end=' ')
+    print("n:", len(lst))
+
+
+def read_files(lst):
+    combined = []
+    for i in range(1,len(lst)):
+        file = open(lst[i], "r")
+        data = file.readlines()
+        num_data = [int(i) for i in data if (i.strip().isnumeric()) ]
+        combined += num_data
+
+        print("Statistics Summary")
+        print_data(num_data)
+
+        file.close()
+
+    return combined
+
+
+
 if (len(system_arv) <= 1):
     print("Please specify the file name")
 else:
     try:
-        file = open(system_arv[1], "r")
-        data = file.readlines()
-        num_data = [int(i) for i in data if (i.strip().isnumeric()) ]
+        combined_data = read_files(system_arv)
+        print("combined")
+        print_data(combined_data)
 
-        print("Statistics Summary")
-        print("mean:", np.mean(num_data))
-        print("std:", np.std(num_data))
-        print("min", np.min(num_data))
-        print("max", np.max(num_data))
     except FileNotFoundError:
         print("No such file")
